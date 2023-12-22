be41f8f77a1e9d29aeafba28bb9cd658805b3784
Israel Akintunde <israelakintunde005@gmail.com>
2023-12-22 18:09:37 +0100

    substituted styled-components for css-modules

---
+++ data/theme.js
@@ -0,0 +1,18 @@
+const colors = {
+  veryDarkGrey: '#363636',
+  darkGrey: '#070707',
+  varyDarkBlue: '#111112',
+  white: '#FFFFFF',
+  lightRed: '#FF6058',
+  darkRed: '#951F1F',
+  lightOrange: '#FEBC30',
+  darkOrange: '#C38400',
+  lightLime: '#29C941',
+  darkLime: '#008414',
+}
+
+const theme = {
+  colors,
+}
+
+export default theme
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<!--import { defineComponent } from 'vue'-->
+
+<!--import ThemeProvider from '@/layouts/ThemeLayout.vue'-->
+
+<!--export default defineComponent({-->
+<!--  setup() {-->
+<!--    onMounted(() => {-->
+<!--      console.log('men mount')-->
+<!--    })-->
+<!--  },-->
+<!--  render() {-->
+<!--    return (-->
+<!--      <ThemeProvider>-->
+<!--        <div>another testing jsx</div>-->
+<!--      </ThemeProvider>-->
+<!--    )-->
+<!--  },-->
+<!--})-->
+
+<script setup></script>
+
+<template>
+  excellent
+</template>
