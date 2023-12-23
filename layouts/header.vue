00c5a016f9839d7e8a7ea02a5d7ee9f54c21603a
Israel Akintunde <israelakintunde005@gmail.com>
2023-12-23 14:18:45 +0100

    sorted header and footer components

---
+++ layouts/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header></header>
+</template>
+
+<style scoped lang="scss">
+header {
+  padding: 1rem;
+  width: 100%;
+  background-color: #{$darkGrey};
+  border: 1px solid #{$blackGrey};
+  border-radius: 2rem;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,14 @@ export default defineNuxtConfig({
       enabled: true,
     },
   },
-  css: ['~/assets/css/normalize.css'],
+  css: ['~/assets/css/normalize.css' ,'~/assets/scss/global.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "~/assets/scss/variables.scss";'
+        }
+      }
+    }
+  }
 })
